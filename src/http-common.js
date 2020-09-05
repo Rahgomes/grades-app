47e8c098ae8655423219ae8e38c5890d9fbde660
Ramon Gomes <ramon_gds@hotmail.com>
2020-09-05 15:36:57 -0300

    Grades app React

---
+++ src/http-common.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+//Define a URL base da origem para consumo do servico
+export default axios.create({
+  baseURL: 'http://localhost:3001/',
+  // baseURL: 'http://localhost:8081/',
+  headers: {
+    'Content-type': 'application/json',
+  },
+});
