0c673d6e1ff92021ca28ed8563597ba9561f6355
Ramon Gomes <ramon_gds@hotmail.com>
2020-09-05 20:41:40 -0300

    Configuração servidor

--- src/http-common.js
+++ src/http-common.js
@@ -1,10 +1,11 @@
 import axios from 'axios';
+import dotenv from 'dotenv';
+dotenv.config();
 
 //Define a URL base da origem para consumo do servico
 export default axios.create({
-  baseURL: 'http://localhost:3001/',
-  // baseURL: 'http://localhost:8081/',
+  baseURL: process.env.BASE_LOCAL_URL || process.env.BASE_EXTERNAL_URL,
   headers: {
-    'Content-type': 'application/json',
+    'Content-type': 'application/json'
   },
 });
