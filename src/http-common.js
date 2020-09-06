cba64f03d26b9a6adf34d162f99e39221aca4e13
Ramon Gomes <ramon_gds@hotmail.com>
2020-09-05 23:12:51 -0300

    Finalizando conexão com o backend

--- src/http-common.js
+++ src/http-common.js
@@ -3,11 +3,9 @@ import axios from 'axios';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const BASE_URL = "http://localhost:3001/" || "https://api-grades-estudantes.herokuapp.com";
-
 //Define a URL base da origem para consumo do servico
 export default axios.create({
-  baseURL: BASE_URL,
+  baseURL: "https://api-grades-estudantes.herokuapp.com",
   headers: {
     'Content-type': 'application/json'
   },
