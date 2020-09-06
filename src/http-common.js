bb8c31427ccb2a6b9e67392be790384a571f163b
Ramon Gomes <ramon_gds@hotmail.com>
2020-09-05 23:09:42 -0300

    Finalizando conexão com o backend

--- src/http-common.js
+++ src/http-common.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const BASE_URL = process.env.BASE_EXTERNAL_URL || process.env.BASE_LOCAL_URL;
+const BASE_URL = `${process.env.BASE_EXTERNAL_URL}` || `${process.env.BASE_LOCAL_URL}`;
 
 //Define a URL base da origem para consumo do servico
 export default axios.create({
